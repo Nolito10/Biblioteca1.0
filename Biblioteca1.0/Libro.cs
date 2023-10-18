ede2ade6105351c9bd00230d604808b377b0c9de
Nolito10 <norlanreyes14rosales@gmail.com>
2023-10-17 20:40:34 -0600

    Agregar archivos de proyecto.

---
+++ Biblioteca1.0/Libro.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca1._0
+{
+    internal class Libro
+    {
+        //Campos estaticos para contadores
+
+        public static int TotalLibros = 0;
+        public static int TotalCopias = 0;
+
+        //Propiedades del Libro
+
+        private string _Isbn;
+        private string _Titulo;
+        private string _Autor;
+        private bool _Estado;
+        private int _CopiasDisponibles;
+        private int _CopiasPrestadas;
+
+        //Getters y Setters
+        public int CopiasPrestadas
+        {
+            get { return _CopiasPrestadas; }
+            set { _CopiasPrestadas = value; }
+        }
+
+
+        public int CopiasDisponibles
+        {
+            get { return _CopiasDisponibles; }
+            set { _CopiasDisponibles = value; }
+        }
+
+
+        public bool Estado
+        {
+            get { return _Estado; }
+            set { _Estado = value; }
+        }
+
+
+        public string Autor
+        {
+            get { return _Autor; }
+            set { _Autor = value; }
+        }
+
+
+
+        public string Titulo
+        {
+            get { return _Titulo; }
+            set { _Titulo = value; }
+        }
+
+
+        public string Isbn
+        {
+            get { return _Isbn; }
+            set { _Isbn = value; }
+        }
+
+        //Constructores
+
+        public Libro()
+        {
+            TotalLibros++;
+        }
+
+        public Libro(string Isbn, string Titulo, string Autor, int Copias)
+        {
+            this._Isbn = Isbn;
+            this._Titulo = Titulo;
+            this._Autor = Autor;
+            this._Estado = true;
+            this._CopiasDisponibles = Copias;
+            TotalLibros++;
+            TotalCopias += Copias;
+        }
+
+        //Metodos de Negocio
+        public bool Prestamo()
+        {
+            if (_CopiasDisponibles > 0)
+            {
+                _CopiasPrestadas++;
+                _CopiasDisponibles--;
+                return true;
+
+            }
+            return false;
+        }
+
+        public bool Devolucion()
+        {
+            if (_CopiasPrestadas > 0)
+            {
+                _CopiasPrestadas--;
+                _CopiasDisponibles++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Inactivar()
+        {
+            if (_CopiasPrestadas == 0)
+            {
+                _Estado = false;
+                return true;
+            }
+            return false;
+        }
+
+        public void Mostrar_Libros(bool EstadoConsulta)
+        {
+            if (_Estado == EstadoConsulta)
+            {
+                Console.WriteLine("ISBN:" + _Isbn);
+                Console.WriteLine("Titulo:" + _Titulo);
+                Console.WriteLine("Autor:" + _Autor);
+                Console.WriteLine("Estado" + _Estado);
+                Console.WriteLine("Copias Disponibles" + _CopiasDisponibles);
+                Console.WriteLine("Copias Prestadas" + _CopiasPrestadas);
+            }
+        }
+
+        public void MostraLibro(string IsbnBusqueda)
+        {
+            if (_Isbn == IsbnBusqueda)
+            {
+                Console.WriteLine("ISBN:" + _Isbn);
+                Console.WriteLine("Titulo:" + _Titulo);
+                Console.WriteLine("Autor:" + _Autor);
+                Console.WriteLine("Estado" + _Estado);
+                Console.WriteLine("Copias Disponibles" + _CopiasDisponibles);
+                Console.WriteLine("Copias Prestadas" + _CopiasPrestadas);
+            }
+            else
+            {
+                Console.WriteLine("No se encontro el Libro con ese ISBN");
+            }
+
+        }
+
+        public static void Informativo()
+        {
+            Console.WriteLine("Total de Libros:" + Libro.TotalLibros);
+            Console.WriteLine("Total de Copias:" + Libro.TotalCopias);
+        }
+
+
+    }
+
+
+}
+
---
+++ Biblioteca1.0/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Libro> libros = new List<Libro>();
+
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Agregar libro");
+                Console.WriteLine("2. Prestar libro");
+                Console.WriteLine("3. Devolver libro");
+                Console.WriteLine("4. Inactivar libro");
+                Console.WriteLine("5. Mostrar libros activos");
+                Console.WriteLine("6. Mostrar libro por ISBN");
+                Console.WriteLine("7. Información general");
+                Console.WriteLine("8. Salir");
+
+                Console.Write("Ingrese una opción: ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        // Agregar libro
+                        Console.Write("ISBN: ");
+                        string _isbn = Console.ReadLine();
+                        Console.Write("Título: ");
+                        string _titulo = Console.ReadLine();
+                        Console.Write("Autor:");
+                        string _autor = Console.ReadLine();
+                        Console.Write("Número de copias: ");
+                        int _numCopias = int.Parse(Console.ReadLine());
+
+                        Libro libro = new Libro(_isbn, _titulo, _autor, _numCopias);
+                        libros.Add(libro);
+                        break;
+                    case 2:
+                        //prestar libro
+                        Console.Write("Ingrese el ISBN del libro a prestar: ");
+                        string isbnPrestamo = Console.ReadLine();
+
+                        Libro libroPrestamo = libros.Find(x => x.Isbn == isbnPrestamo);
+                        if (libroPrestamo != null)
+                        {
+                            if (libroPrestamo.Prestamo())
+                            {
+                                Console.WriteLine("Préstamo realizado");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No hay copias disponibles para prestar");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró el libro con ese ISBN");
+                        }
+                        break;
+
+                    case 3:
+                        //Devolver libro
+                        Console.Write("Ingrese el ISBN del libro a devolver: ");
+                        string isbnDevolucion = Console.ReadLine();
+
+                        Libro libroDevolucion = libros.Find(x => x.Isbn == isbnDevolucion);
+                        if (libroDevolucion != null)
+                        {
+                            if (libroDevolucion.Devolucion())
+                            {
+                                Console.WriteLine("Devolución realizada");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Este libro no está prestado actualmente");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró el libro con ese ISBN");
+                        }
+
+                        break;
+                    case 4:
+                        //Inactivar libro
+                        Console.Write("Ingrese el ISBN del libro a inactivar: ");
+                        string isbnInactivar = Console.ReadLine();
+
+                        Libro libroInactivar = libros.Find(x => x.Isbn == isbnInactivar);
+                        if (libroInactivar != null)
+                        {
+                            if (libroInactivar.Inactivar())
+                            {
+                                Console.WriteLine("Libro inactivado");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No se puede inactivar porque tiene copias prestadas");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró el libro con ese ISBN");
+                        }
+
+                        break;
+                    case 5:
+                        //Mostrar libros activos
+                        foreach (Libro Libro in libros)
+                        {
+                            if (Libro.Estado)
+                            {
+                                Libro.Mostrar_Libros(true);
+                            }
+                        }
+
+                        break;
+                    case 6:
+                        // Mostrar libro por ISBN
+                        Console.Write("Ingrese el ISBN a buscar: ");
+                        string isbnBusqueda = Console.ReadLine();
+
+                        Libro libroEncontrado = libros.Find(x => x.Isbn == isbnBusqueda);
+                        if (libroEncontrado != null)
+                        {
+                            libroEncontrado.MostraLibro(isbnBusqueda);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se encontró el libro con ese ISBN");
+                        }
+
+                        break;
+                    case 7:
+                        Libro.Informativo();
+                        break;
+                    case 8:
+
+                        salir = true;
+                        break;
+
+                    default:
+                        Console.WriteLine(" ¡HERROR!, OPCION FUERA DE RANGO."); break;
+
+                }
+
+            }
+        }
+    }
+
+}
